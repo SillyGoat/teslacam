58d3bd024076c8fffa635b6736ae6bc845bdd99e
SillyGoat <SillyGoat@users.noreply.github.com>
2019-10-29 01:39:11 -0700

    Support day units

--- teslacam/time_duration.py
+++ teslacam/time_duration.py
@@ -6,7 +6,9 @@ def seconds_to_units(seconds):
     ' Convert seconds into days, hours, minutes, seconds '
     minutes, seconds = divmod(seconds, 60)
     hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
     return Units(
+        Unit(int(days), 'day'),
         Unit(int(hours), 'hour'),
         Unit(int(minutes), 'minute'),
         Unit(int(seconds), 'second'),
