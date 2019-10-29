bf300aaa0711b02d17c788ff0c9614d273d8865a
SillyGoat <SillyGoat@users.noreply.github.com>
2019-10-29 02:19:23 -0700

    Reduce option Expose DONT_REDUCE to the api Document the layout field types

--- teslacam/__init__.py
+++ teslacam/__init__.py
@@ -1,4 +1,5 @@
 ' Client API '
 
+from .constants import DONT_REDUCE
 from .custom_types import FFMpegPaths, LayoutOptions, BaseFolderPaths
 from .extract import extract_videos
--- teslacam/custom_types.py
+++ teslacam/custom_types.py
@@ -3,9 +3,15 @@ import collections
 
 LayoutOptions = collections.namedtuple(
     'LayoutOptions',
-    ['codec', 'preset', 'layout', 'reduce']
+    [
+        'codec', # Codec as a string
+        'preset', # Codec preset as a string
+        'layout', # Layout name as a string
+        'reduce', # Percentage value from 1 to 100 as a float
+    ]
 )
 
+# All structures here hold full filepaths
 FFMpegPaths = collections.namedtuple(
     'FFMpegPaths',
     ['ffprobe', 'ffmpeg']
